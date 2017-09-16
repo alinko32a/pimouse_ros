5fd38ca33a483ab345bff218f550d303087e1853
hideaki ikemoto <hideaki.ikemoto12@gmail.com>
2017-09-16 17:57:10 +0900

    test lightsensors 2

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -46,7 +46,7 @@ class LightsensorTest(unittest.TestCase):
 	time.sleep(3)
 	
 	self.assertTrue(self.count < c_prev + 4, "freq does not change")
-	self.assertFales(self.count == c_prev, "subscriber is stopped")
+	self.assertFalse(self.count == c_prev, "subscriber is stopped")
 
 if __name__ == '__main__':
     time.sleep(3)
